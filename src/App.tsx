879ca00cf38ea8a4fe557dfe0d82cdd2f164ba39
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-07 10:05:35 +0530

    Test React routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import Nav from "./components/nav/Nav";
 import Airport from "./components/airport/Airport";
 import { Flights } from "./components/flights/Flights";
 import { Login } from "./components/login/Login";
-import {Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-  <>
-  <Nav />
+    <>
+      <Nav />
 
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/airports" element={<Airport />} />
         <Route path="/flights" element={<Flights />} />
       </Routes>
-
-  </>
+    </>
   );
 }
 
